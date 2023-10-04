32d74ef2afe7913cecbbbe1945d964cff1288b54
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-10-04 21:49:42 +0530

    Dictionary

--- dictionary/script.js
+++ dictionary/script.js
@@ -11,25 +11,33 @@ button.addEventListener("click", () =>{
     fetch(`${url}${inputWord}`)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data[0]);
+        console.log(data);
         result.innerHTML = `
             <div class="word">
                 <h3>${inputWord}</h3>
-                <button><i class="fas fa-volume-up"></i></button>
+                <button onclick="playSound()" ><i class="fas fa-volume-up"></i></button>
             </div>
 
             <div class="details">
                 <p>${data[0].meanings[0].partOfSpeech}</p>
-                <p>${data[0].phonetics}</p>
+                <p>${data[0].phonetics[1].text}/</p>
             </div>
 
             <p class="meaning">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                Impedit, officiis.
+                ${data[0].meanings[0].definitions[0].definition}
             </p>
 
-            <p class="example">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                 Temporibus, voluptates?</p>`;
-    } );
+            <p class="synonyms">
+                ${data[0].meanings[0].synonyms[0]}
+            </p>`;
+
+            sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
+            console.log(sound);
+    }).catch( () =>{
+            result.innerHTML =`<h3 class="error" >Couldn't found the word</h3>`
+    })
 });
+
+function playSound(){
+    sound.play();
+}
